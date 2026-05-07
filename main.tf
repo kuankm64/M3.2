e33aa96b05b60ec81cdb5453b94defc3b6f1a639
kuankm64 <kuan.kkm@gmail.com>
2026-05-07 20:06:39 +0800

    adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce12-tfstate" # Change this
+    key    = "kuankm/terraform.tfstate"  # Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =   "kuankm-tfstate-"
+}
