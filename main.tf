fa0800857f9d1bfad46c8ce11ecaa5570562367b
kuankm64 <kuan.kkm@gmail.com>
2026-05-07 20:15:05 +0800

    to fix terraform init error

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "sctp-ce12-tfstate" # Change this
-    key    = "kuankm/terraform.tfstate"  # Change this
+    bucket = "sctp-ce12-tfstate"        # Change this
+    key    = "kuankm/terraform.tfstate" # Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =   "kuankm-tfstate-"
+  bucket_prefix = "kuankm-tfstate-"
 }
