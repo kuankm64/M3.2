3d8ce70b66286518bd37672bf8fdf01d7056bf33
kuankm64 <kuan.kkm@gmail.com>
2026-05-09 09:47:42 +0800

    skip checkov error

--- main.tf
+++ main.tf
@@ -21,13 +21,13 @@ terraform {
   }
 }
 resource "aws_s3_bucket" "s3_tf" {
-  # checkov:skip=CKV2_AWS_62: Event bridge notification is not required
-  # checkov:skip=CKV2_AWS_6: Public access block is handled by account-level settings
-  # checkov:skip=CKV2_AWS_61: Lifecycle configuration not needed for state files
-  # checkov:skip=CKV2_AWS_144: Cross-region replication not required
-  # checkov:skip=CKV2_AWS_21: Logging is not required for this bucket
-  # checkov:skip=CKV2_AWS_145: Encryption is handled by default S3 encryption
-  # checkov:skip=CKV2_AWS_18: Access logging not required
+  # checkov:skip=CKV_AWS_62: Event bridge notification is not required
+  # checkov:skip=CKV_AWS_6: Public access block is handled by account-level settings
+  # checkov:skip=CKV_AWS_61: Lifecycle configuration not needed for state files
+  # checkov:skip=CKV_AWS_144: Cross-region replication not required
+  # checkov:skip=CKV_AWS_21: Logging is not required for this bucket
+  # checkov:skip=CKV_AWS_145: Encryption is handled by default S3 encryption
+  # checkov:skip=CKV_AWS_18: Access logging not required
 
   bucket_prefix = "kuankm-tfstate-"
   force_destroy = true
